06511334dc78ec8f34ebf1ddc0aaa7ded59bed44
Satyam2192 <Satyam21092@gmail.com>
2023-05-17 17:37:49 +0530

    test

--- src/components/recipe-iteam/index.jsx
+++ src/components/recipe-iteam/index.jsx
@@ -1,26 +1,27 @@
-import "./style.css"
+import "./style.css";
 
 const RecipeIteam = (props) => {
+  const { id, image, title, addToFavorites } = props;
 
-    const { id, image, title, addToFavorites } = props;
+  console.log("Recipe item props:", props);
 
-    console.log("Recipe item props:", props);
+  const handleAddToFavorites = () => {
+    console.log("Adding to favorites:", props);
+    addToFavorites();
+  };
 
-    const handleAddToFavorites = () => {
-        console.log("Adding to favorites:", props);
-        addToFavorites();
-    }
+  return (
+    <div key={id} className="recipe-item">
+      <div>
+        <img src={image} alt="recipe" />
+      </div>
+      <p>{title}</p>
 
-    return (
-        <div key={id} className="recipe-item">
-            <div>
-                <img src={image} alt="image of recipe" />
-            </div>
-            <p>{title}</p>
-
-            <button type="button" onClick={handleAddToFavorites}>Add to Favorites</button>
-        </div>
-    )
-}
+      <button type="button" onClick={handleAddToFavorites}>
+        Add to Favorites
+      </button>
+    </div>
+  );
+};
 
 export default RecipeIteam;
